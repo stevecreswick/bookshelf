d3dd3650789420aa631f70a28665a81d5d78fbd9
Steve Creswick <screswick@gmail.com>
2018-12-28 12:41:12 -0500

    Fix port for heroku

--- server.js
+++ server.js
@@ -48,7 +48,7 @@ authors.forEach((author) => {
   });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
 });
